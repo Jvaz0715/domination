1f18f6c7cc415dbf7ecb5668154e0d56b7b1f0f9
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-18 12:04:18 -0400

    Solves Part I, problesm 3 and 4

--- main.js
+++ main.js
@@ -11,3 +11,19 @@ function lineThrough() {
 // call lineThrough function to effect firstItem
 
 lineThrough();
+
+// write a function that takes in a string representing a dom ELEMENT's id and a string presenting a url. the url should set the image with that id to have that url as its image source
+
+function addImage (imageID, url) {
+    let image = document.querySelector(imageID);
+
+    image.src = url;
+}
+
+// call three times to add three images
+
+addImage('#image-1', 'https://images.unsplash.com/photo-1603993097397-89c963e325c7?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80')
+
+addImage('#image-2', 'https://images.unsplash.com/photo-1521063512073-432ce10aed90?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80')
+
+addImage('#image-3', 'https://images.unsplash.com/photo-1505153695651-9366147105f3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80')
